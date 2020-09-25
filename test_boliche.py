a8815b123f0597043658720515e4b9be9bba626f
ThalesCSoares <th4les.castro@gmail.com>
2020-09-25 17:53:26 -0300

    Refatorando entrada de teste.

--- test_boliche.py
+++ test_boliche.py
@@ -1,16 +1,19 @@
 # import pytest
 from boliche import boliche_game
 
+list_input = lambda x,arr_size: [x for i in range(arr_size)]
+
 class TestBoliche:
-    def test_score0(self):
+
+    def test_score_all_0(self):
         result = 0
-        input_score = list(0 for i in range(20))
+        input_score = list_input(0, 20)
         score = boliche_game(input_score)
         assert result == score
     
     def test_score_all_1(self):
         result = 20
-        input_score = list(1 for i in range(20))
+        input_score = list_input(1,20)
         score = boliche_game(input_score)
         assert result == score
-        
+    
