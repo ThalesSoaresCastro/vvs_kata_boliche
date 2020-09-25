65366247410752c96ebb4089447863d0187a4dbc
ThalesCSoares <th4les.castro@gmail.com>
2020-09-25 19:35:14 -0300

    Refactor: Primeira parte da refatoraçao do test.

--- boliche.py
+++ boliche.py
@@ -1,6 +1,12 @@
-def boliche_game(value):
-    result = 0
-    for v in value:
-        result+=v
 
-    return result
+class Boliche:
+    plays_game = [0 for i in range(20)]
+    actual_played = 0
+
+    def play_score(self):
+        return sum(self.plays_game)
+
+
+    def play(self,value):
+        self.plays_game[self.actual_played] = value
+        self.actual_played+=1
--- test_boliche.py
+++ test_boliche.py
@@ -1,19 +1,27 @@
-# import pytest
-from boliche import boliche_game
+import pytest
+from boliche import Boliche
 
-list_input = lambda x,arr_size: [x for i in range(arr_size)]
+play_boliche = lambda b,x,arr_size: [b.play(x) for i in range(arr_size)]
 
 class TestBoliche:
 
     def test_score_all_0(self):
+        b=Boliche()
         result = 0
-        input_score = list_input(0, 20)
-        score = boliche_game(input_score)
+        input_score = play_boliche(b,0, 20)
+        score = b.play_score()
         assert result == score
-    
+        
     def test_score_all_1(self):
+        b=Boliche()
         result = 20
-        input_score = list_input(1,20)
-        score = boliche_game(input_score)
+        input_score = play_boliche(b,1,20)
+        score = b.play_score()
         assert result == score
+        
+    # def test_spare_score_16(self):
+    #     result = 16
+    #     input_score = list_input()
+    #     score = boliche_game(input_score)
+    #     assert result == score
     
