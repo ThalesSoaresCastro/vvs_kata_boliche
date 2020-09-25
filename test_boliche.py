4fcaab137d185c616d7bf283ad8ea39d26ca722c
ThalesCSoares <th4les.castro@gmail.com>
2020-09-25 19:45:48 -0300

    Refactor: Refatoraçao do objeto e funçao de chamada para as jogadas - OK.

--- test_boliche.py
+++ test_boliche.py
@@ -1,22 +1,25 @@
 import pytest
 from boliche import Boliche
 
+
 play_boliche = lambda b,x,arr_size: [b.play(x) for i in range(arr_size)]
 
 class TestBoliche:
 
-    def test_score_all_0(self):
-        b=Boliche()
+    @pytest.fixture(autouse=True)
+    def boliche(self):
+        return Boliche()
+
+    def test_score_all_0(self, boliche):
         result = 0
-        input_score = play_boliche(b,0, 20)
-        score = b.play_score()
+        input_score = play_boliche(boliche,0, 20)
+        score = boliche.play_score()
         assert result == score
         
-    def test_score_all_1(self):
-        b=Boliche()
+    def test_score_all_1(self, boliche):
         result = 20
-        input_score = play_boliche(b,1,20)
-        score = b.play_score()
+        input_score = play_boliche(boliche,1,20)
+        score = boliche.play_score()
         assert result == score
         
     # def test_spare_score_16(self):
