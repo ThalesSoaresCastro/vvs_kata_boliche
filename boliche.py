fdce7a8b754747a4fb5945e87305e529d8a35c79
ThalesCSoares <th4les.castro@gmail.com>
2020-09-25 21:33:13 -0300

    Strike feito..

--- boliche.py
+++ boliche.py
@@ -10,16 +10,26 @@ class Boliche:
         [sum_value, index]=[0,0]
 
         for i in range(0, 10):
-            if self.spare_verify(index) :
+            
+            if self.strike_verify(index):
+                sum_value = 10 + self.plays_game[index+1] + self.plays_game[index+2]
+                index+=1
+
+            elif self.spare_verify(index) :
                 sum_value+= 10 + self.plays_game[i+2]
                 index+=2
+
             else:
-                sum_value+=self.plays_game[index]+self.plays_game[(index+1)]
+                sum_value += self.plays_game[index]+self.plays_game[(index+1)]
                 index+=2
+
         return sum_value
 
     def spare_verify(self,frame):
         return (self.plays_game[frame]+self.plays_game[(frame+1)]) == 10
+
+    def strike_verify(self, frame):
+        return self.plays_game[frame] == 10
 
     def play(self,value):
         self.plays_game[self.actual_played] = value
--- test_boliche.py
+++ test_boliche.py
@@ -3,6 +3,7 @@ from boliche import Boliche
 
 
 play_boliche = lambda b,x,arr_size: [b.play(x) for i in range(arr_size)]
+  
 
 class TestBoliche:
 
@@ -24,9 +25,24 @@ class TestBoliche:
 
     def test_spare_score_16(self, boliche):
         result = 16
-        boliche.play(5)
-        boliche.play(5)
+        """
+            Rolando o spare:
+                chama play() com valor 5 duas vezes seguidas
+                e em seguida chama o play() mais uma vez mas com valor 3. 
+        """
+        play_boliche(boliche,5,2)
         boliche.play(3)
         score = boliche.play_score()
         assert result == score
     
+    def test_strike_score_24(self, boliche):
+        result = 24
+        
+        boliche.play(10)
+        boliche.play(3)
+        boliche.play(4)
+        play_boliche(boliche,0,16)
+        score = boliche.play_score()
+
+        assert result == score
+
