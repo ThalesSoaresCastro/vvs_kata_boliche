e09d386861cf53f4e10f0b29593dea93dbf4bb20
ThalesCSoares <th4les.castro@gmail.com>
2020-09-25 21:11:40 -0300

    Teste de spare feito.

--- boliche.py
+++ boliche.py
@@ -1,21 +1,32 @@
 
 class Boliche:
-    plays_game = [0 for i in range(20)]
-    actual_played = 0
+    #plays_game = [0 for i in range(21)]
+    #actual_played = 0
+    def __init__(self):
+        self.plays_game = [0 for i in range(21)]
+        self.actual_played = 0
 
     def play_score(self):
         [sum_value, index]=[0,0]
 
-        #frame 0-9 com passo igual à 2.
-        #for i in range(0,20,2):
         for i in range(0, 10):
-            sum_value+=self.plays_game[index]+self.plays_game[(index+1)]
-            index+=2
-            #sum_value = self.plays_game[i+1]+self.plays_game[i]
+            if (self.plays_game[index]+self.plays_game[(index+1)]) == 10:
+                sum_value+= 10 + self.plays_game[i+2]
+                index+=2
+            else:
+                sum_value+=self.plays_game[index]+self.plays_game[(index+1)]
+                index+=2
         return sum_value
-        #return sum(self.plays_game)
 
 
     def play(self,value):
         self.plays_game[self.actual_played] = value
         self.actual_played+=1
+
+
+# b=Boliche()
+
+# b.play(5)
+# b.play(5)
+# b.play(3)
+# print(b.play_score())
--- test_boliche.py
+++ test_boliche.py
@@ -6,25 +6,27 @@ play_boliche = lambda b,x,arr_size: [b.play(x) for i in range(arr_size)]
 
 class TestBoliche:
 
-    @pytest.fixture(autouse=True)
+    @pytest.fixture(scope='function')
     def boliche(self):
         return Boliche()
 
     def test_score_all_0(self, boliche):
         result = 0
-        input_score = play_boliche(boliche,0, 20)
+        play_boliche(boliche,0, 20)
         score = boliche.play_score()
         assert result == score
-        
+    
     def test_score_all_1(self, boliche):
         result = 20
-        input_score = play_boliche(boliche,1,20)
+        play_boliche(boliche,1,20)
         score = boliche.play_score()
         assert result == score
-        
-    # def test_spare_score_16(self):
-    #     result = 16
-    #     input_score = list_input()
-    #     score = boliche.play_score()
-    #     assert result == score
+
+    def test_spare_score_16(self, boliche):
+        result = 16
+        boliche.play(5)
+        boliche.play(5)
+        boliche.play(3)
+        score = boliche.play_score()
+        assert result == score
     
