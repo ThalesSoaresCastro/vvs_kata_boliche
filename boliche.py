65c7228e5c7759fd97fe6601097509dcfbb3711b
ThalesCSoares <th4les.castro@gmail.com>
2020-09-25 21:18:16 -0300

    Refatoraçao da verificaçao de frame ser ou nao spare.

--- boliche.py
+++ boliche.py
@@ -10,7 +10,7 @@ class Boliche:
         [sum_value, index]=[0,0]
 
         for i in range(0, 10):
-            if (self.plays_game[index]+self.plays_game[(index+1)]) == 10:
+            if self.spare_verify(index) :
                 sum_value+= 10 + self.plays_game[i+2]
                 index+=2
             else:
@@ -18,6 +18,8 @@ class Boliche:
                 index+=2
         return sum_value
 
+    def spare_verify(self,frame):
+        return (self.plays_game[frame]+self.plays_game[(frame+1)]) == 10
 
     def play(self,value):
         self.plays_game[self.actual_played] = value
